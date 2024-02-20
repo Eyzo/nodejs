57e9900562ab411fe1915167f21ee40253a7ce77
Tony Duhamel <tduhamel@insitaction.com>
2024-02-20 17:59:39 +0100

    add create/update

--- Router/MemberRouter.js
+++ Router/MemberRouter.js
@@ -1,5 +1,6 @@
 const utils = require('./../Utils/ResponseUtils');
 const express = require('express');
+const fs = require('fs');
 
 /**
  * @type {Array.<{id: Number, first_name: String, last_name: String}>} members
@@ -21,7 +22,32 @@ router.post('/create', (req, res) => {
     first_name: req.body.first_name,
     last_name: req.body.last_name
   });
-  res.json(utils.responseSuccess('Ajout bien effectué', members));
+  const data = JSON.stringify(members);
+  fs.writeFile('./Data/members.json', data, (err) => {
+    res.json(utils.responseSuccess('Ajout bien effectué', members));
+  });
+});
+router.put('/update/:id', (req, res) => {
+  const {id} = req.params;
+  const member = members[id - 1];
+  const {first_name, last_name} = req.body
+  member.first_name = first_name;
+  member.last_name = last_name;
+  const data = JSON.stringify(members);
+  fs.writeFile('./Data/members.json', data, (err) => {
+    res.json(utils.responseSuccess('Modification bien effectué', members));
+  });
+});
+
+router.delete('/delete/:id', (req, res) => {
+  const {id} = req.params;
+  const data = JSON.stringify(members);
+  members.splice(id - 1, 1);
+  console.log(members);
+  // fs.writeFile('./Data/members.json', data, (err) => {
+  //   res.json(utils.responseSuccess('Modification bien effectué', members));
+  // });
+  res.json('ok');
 });
 
 module.exports = router;
